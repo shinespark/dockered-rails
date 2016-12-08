a2e424c3e096a992e7621c45fa2026b3a3dce906
shinespark <shinespark64@gmail.com>
2016-12-08 13:57:45 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var HTML_SRC  = './**/*.html';
+var HTML_DEST = './';
+var SCSS_SRC  = './assets/stylesheets/**/*.scss';
+var CSS_SRC   = './css/**/*.css';
+var CSS_DEST  = './css/';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var scsslint = require('gulp-scss-lint');
+var bs = require('browser-sync').create();
+
+// browser-sync
+gulp.task('bs', function(){
+  var bsOptions = {};
+  bsOptions.files = [HTML_SRC, CSS_SRC];
+  bsOptions.proxy = 'localhost:3000';
+  bsOptions.port  = 3001;
+  bs.init(bsOptions);
+});
+
+
+// SCSS
+gulp.task('sass', function(){
+  return gulp.src(SCSS_SRC)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(CSS_DEST));
+});
+
+
+// scss-lint
+gulp.task('scss-lint', function(){
+  return gulp.src(SCSS_SRC)
+    .pipe(scsslint());
+});
+
+
+// scss-watch
+gulp.task('scss:watch', function(){
+  var watcher = gulp.watch(SCSS_SRC, ['scss-lint', 'sass']);
+  watcher.on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+});
+
+gulp.task('default', ['bs', 'scss:watch']);
