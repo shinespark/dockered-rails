c651e2b0c9e627c09b0d6f15faf8acc81254ee45
shinespark <shinespark64@gmail.com>
2018-09-11 18:23:03 +0900

    標準化

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/test', to: 'static_pages#test'
 
-  root to: 'static_pages#index'
+  root to: 'static_pages#test'
   resources :text_garblings, only: %i(index)
 end
